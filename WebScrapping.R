2a2ea09c5aaff211a43a334a44d418a1546c2d3e
Ale-coding <eagarciamonzon@gmail.com>
2022-07-30 07:47:57 -0500

    Scrapping

---
+++ WebScrapping.R
@@ -0,0 +1,29 @@
+rm(list=ls())
+getwd()
+library(stringr)
+library(plyr)
+library(ggvis)
+library(knitr)
+library(rvest)
+library(tidyverse)
+library(glue)
+library(dplyr)
+library(jsonlite)
+library("writexl")
+
+x= "https://origin.cpc.ncep.noaa.gov/products/analysis_monitoring/ensostuff/ONI_v5.php"
+#tabla <- read_html(x)%>% html_table() %>% .[[1]]
+tabla <- x %>%  read_html() %>%html_nodes(xpath='/html/body/table[4]/tbody/tr/td[2]/table[2]') %>% html_table()
+tabla <- tabla[[1]]
+colnames(tabla)=tabla[c(1),]
+tabla=tabla[-c(1),]
+tabla<- subset(tabla, Year != "Year")
+tabla<- subset(tabla, Year >= 2010)
+
+henry <- "https://www.eia.gov/dnav/ng/hist/rngwhhdm.htm" 
+tabla_henry <- x %>%  read_html() %>%html_nodes(xpath='/html/body/table[3]') %>% html_table()
+tabla_henry <- tabla_henry[[1]]
+
+
+  
+  "/html/body/table[3]/tbody"
