49e5e683846f784880255552a2ff24555daecf9c
Andres T <ag.torresc1@uniandes.edu.co>
2022-08-02 21:45:02 -0500

    Graficas

--- ARIMA.R
+++ ARIMA.R
@@ -5,6 +5,7 @@ setwd("D:/Documents/Andres/ANDES/2.5/ProyectoFinalG2")
 
 #1.  Librerias----
 #install.packages("lubridate")
+#install.packages("forecast")
 require(pacman)
 p_load(tidyverse, # manipular/limpiar conjuntos de datos
        readxl,    # carga datos de excel
@@ -13,7 +14,7 @@ p_load(tidyverse, # manipular/limpiar conjuntos de datos
        astsa,
        forecast,
        foreign,
-       quantmod
+       quantmod,LSTS
        )
 
 #2.  Descargar datos de mi pc ----
@@ -41,18 +42,13 @@ dates <- Pbol$Fecha
 Pbol_2 <- Pbol
 Pbol_2 <- Pbol_2[-1]
 
+##
+
 ## Create a time series object
 
 Arimar.ts <- ts(Pbol_2,     
            start = c(2018,1,1), 
            frequency = 365)
-Arimar.ts
-
-print(Arimar.ts)
-class(Arimar.ts)
-start(Arimar.ts)
-end(Arimar.ts)
-plot(Arimar.ts,  main="Serie de tiempo", ylab="Precio", col="navy")
 
 #Requiere comprobar estacionalidad
 
@@ -67,58 +63,44 @@ adf.test(seriedif,alternative = "stationary")
 acf(ts(seriedif, frequency=1))
 pacf(ts(seriedif, frequency=1))
 
-#7.1 Modelo Arima 1 ----
-## 1 autoregresivo, 1 diferencia, 1 media movil
-modelo1 <- arima(Arimar.ts,order=c(1,1,1))
-summary(modelo1)
-#tsdiag(modelo1)
-Box.test(residuals(modelo1),type="Ljung-Box")
-# con p-vale de 0.7893 mayor a .05 ,se puede afirmar que hay ruido blanco, por consiguiente
+
+#P_Bolsa
+train = subset(Arimar.ts,start = 1,end = 1185) 
+test = subset(Arimar.ts,start =  1186) 
+#validar mejor modelo
+modelo_1 <- auto.arima(train, xreg=train, trace=T, D=1,lambda=0)
+forecast <- forecast(modelo_1, xreg= test) ; forecast
+summary(modelo_1)
+autoplot(train,main = 'Forecast precio de bolsa 01-01-2018 a 31-03-2021',xlab = 'Fecha',
+         ylab = 'Precio bolsa ($/kWh)')+
+  autolayer(forecast,series = 'Precio bolsa predicho')+
+  autolayer(test,series = 'Set de prueba abril 2021 en adelante')
+
+
+Box.test(residuals(modelo_1),type="Ljung-Box")
+
+Box.Ljung.Test(residuals(modelo_1),lag=NULL,main=NULL)
+
+# con p-vale de 0.7131 mayor a .05 ,se puede afirmar que hay ruido blanco, por consiguiente
 # nuestro modelo se ajusta bien.
 # Ruido blanco significa que el error
 # media igual a cero
 # Varianza constante
 # No estar serialmente correlacionada
 
+#7.1 Modelo Arima 1 ----
+
+
 #Mirar errores
-error=residuals(modelo1)
+error=residuals(modelo_1)
 plot(error, main="Error modelo 1", col="navy")
 
-#Pronosticos Arima
-pronostico1_1=forecast::forecast(modelo1,h=30)
+#Pronostico 
+pronostico1_1 <- forecast::forecast(Arimar.ts,xreg=Arimar.ts,h=30)
 pronostico1_1
 plot(pronostico1_1, main="Pronóstico modelo 1 a 30 días", ylab="Precio ($/kWh)", col="navy")
 
-pronostico1_2=forecast::forecast(modelo1,h=60)
-pronostico1_2
-plot(pronostico1_2, main="Pronóstico modelo 1 a 60 días", ylab="Precio ($/kWh)", col="navy")
-
-pronostico1_3=forecast::forecast(modelo1,h=90)
-pronostico1_3
-plot(pronostico1_3,  main="Pronóstico modelo 1 a 90 días", ylab="Precio ($/kWh)", col="navy")
-
-
-#7.2 Modelo Arima 2 ----
-## 1 autoregresivo, 1 diferencia, 7 media movil
-modelo2 <- arima(Arimar.ts,order=c(1,1,7))
-summary(modelo2)
-#tsdiag(modelo2)
-Box.test(residuals(modelo2),type="Ljung-Box")
-# con p-vale de 0.7893 mayor a .05 ,se puede afirmar que hay ruido blanco, por consiguiente
-
-#Mirar errores
-error=residuals(modelo2)
-plot(error, main="Error modelo 2", col="navy")
-
-#Pronosticos Arima
-pronostico2_1=forecast::forecast(modelo2,h=30)
-pronostico2_1
-plot(pronostico2_1, main="Pronóstico modelo 2 a 30 días", ylab="Precio ($/kWh)", col="navy")
-
-pronostico2_2=forecast::forecast(modelo2,h=60)
-pronostico2_2
-plot(pronostico2_2, main="Pronóstico modelo 2 a 60 días", ylab="Precio ($/kWh)", col="navy")
-
-pronostico2_3=forecast::forecast(modelo2,h=90)
-pronostico2_3
-plot(pronostico2_3,  main="Pronóstico modelo 2 a 90 días", ylab="Precio ($/kWh)", col="navy")
+autoplot(train,main = 'Forecast precio de bolsa a 30 dias',xlab = 'Fecha',
+         ylab = 'Precio bolsa ($/kWh)')+
+  autolayer(pronostico1_1,series = 'Precio bolsa predicho')+
+  autolayer(test,series = 'Set de prueba abril 2021 en adelante')
